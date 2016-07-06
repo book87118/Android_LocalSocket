50245d3af91d0dab4686a4b1f84e051179dcb16f
maxlin <book87118@gmail.com>
2016-07-06 17:38:45 -0400

    init

---
+++ app/src/main/java/ching/android_localsocket/MainActivity.java
@@ -0,0 +1,13 @@
+package ching.android_localsocket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
