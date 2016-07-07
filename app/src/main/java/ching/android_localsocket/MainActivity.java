5992b0fa376ef3cf2e93e49c9fb18166b4b28303
maxlin <book87118@gmail.com>
2016-07-06 22:34:14 -0400

    join AddView function,for add the view over the all activities

--- app/src/main/java/ching/android_localsocket/MainActivity.java
+++ app/src/main/java/ching/android_localsocket/MainActivity.java
@@ -2,12 +2,48 @@ package ching.android_localsocket;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import ching.android_localsocket.layout.AddView;
 
 public class MainActivity extends AppCompatActivity {
+    final static String TAG = "MainActivity";
+    private Button bt_connect;
+    private AddView mAddView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt_connect = (Button)findViewById(R.id.bt_connect);
+        bt_connect.setOnClickListener(onClick);
+        setAddView();
+
+
+
     }
+    private void setAddView(){
+        mAddView = new AddView(this);
+        mAddView.setAddView();
+    }
+
+
+
+    View.OnClickListener onClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.bt_connect:
+                    mAddView.showPictures("Up");
+                    break;
+                default:
+                    break;
+
+
+            }
+        }
+    };
 }
---
+++ app/src/main/java/ching/android_localsocket/layout/AddView.java
@@ -0,0 +1,148 @@
+package ching.android_localsocket.layout;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import ching.android_localsocket.R;
+
+/**
+ * Created by book871181 on 15/12/6.
+ * Add a view on top of all the Activities.
+ *
+ */
+public class AddView extends FrameLayout {
+
+    private static String TAG = "AddView";
+    private WindowManager mWindowManager;
+    private WindowManager.LayoutParams mLayoutParams;
+    private View addView;
+    private ImageView imageView;
+
+    private boolean isAddView = false;
+
+
+
+    /**
+    * construct AddView function
+    * */
+    public AddView(Context context) {
+        super(context);
+        addView = LayoutInflater.from(context).inflate(R.layout.addview,this);
+        imageView = (ImageView)addView.findViewById(R.id.imageView);
+
+        setWindowManager(context);
+    }
+
+    /**
+     * setting WindowManager
+     *   NOT touch and focus
+     *
+     * **/
+
+    private void setWindowManager(Context context){
+        mWindowManager = (WindowManager)context.getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        mLayoutParams = new WindowManager.LayoutParams();
+        mLayoutParams.type = WindowManager.LayoutParams.TYPE_TOAST;
+
+        mLayoutParams.format = PixelFormat.TRANSPARENT;
+        mLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
+        mLayoutParams.gravity = 1;
+        mLayoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+        mLayoutParams.height = WindowManager.LayoutParams.MATCH_PARENT;
+
+    }
+
+    private  ObjectAnimator hideAnimator;
+
+
+    /*
+    * setting Aniamtor
+    * */
+
+    private void setAnimator(){
+        hideAnimator = new ObjectAnimator();
+
+        hideAnimator = ObjectAnimator.ofFloat(imageView,"alpha",
+                new float[]{0f,1f,0f});
+
+
+    }
+    /**
+     * When Local Socket Client get Data(String), it will call this function,
+     * And this function will show animatiton  over the screen.
+     * For prove Client get the Data.
+     * */
+
+    public void showPictures(String result){
+        setAnimator();
+        AnimatorSet  mAnimatorSet = new AnimatorSet();
+        //setting show up period
+        mAnimatorSet.setDuration(500);
+
+        mAnimatorSet.setInterpolator(new DecelerateInterpolator());//動畫設置减速
+
+        switch (result){
+            case "Up":
+                imageView.setImageResource(R.drawable.direction_top);
+                mAnimatorSet.play(hideAnimator);
+                mAnimatorSet.start();
+                break;
+
+            case "Down":
+                imageView.setImageResource(R.drawable.direction_down);
+                mAnimatorSet.play(hideAnimator);
+                mAnimatorSet.start();
+                Toast.makeText(getContext(),"Guesture Up!",Toast.LENGTH_SHORT).show();
+
+                break;
+            case "Left":
+                imageView.setImageResource(R.drawable.direction_left);
+                mAnimatorSet.play(hideAnimator);
+                mAnimatorSet.start();
+
+                break;
+
+            case "Right":
+                imageView.setImageResource(R.drawable.direction_right);
+                mAnimatorSet.play(hideAnimator);
+                mAnimatorSet.start();
+
+                break;
+            default:
+                Toast.makeText(getContext(),"Default!",Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+    }
+
+    /**
+     * click two time connect will break connect and also stop add View
+     *
+     * */
+    public void setAddView(){
+
+        if(isAddView){
+            Log.d("gusture","removeView");
+            mWindowManager.removeView(this);
+            isAddView = false;
+        }else{
+            mWindowManager.addView(this,mLayoutParams);
+            isAddView = true;
+
+        }
+    }
+
+}
